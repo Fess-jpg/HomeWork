5adf61a81ecba87b3f1b0fad45ef55bbdef9a5ca
Fess-jpg <maksimnagorny91@gmail.com>
2025-03-22 23:53:37 +0300

    Home 2

--- src/org/skypro/skyshop/Product.java
+++ src/org/skypro/skyshop/Product.java
@@ -19,7 +19,7 @@ public class Product {
 
     @Override
     public String toString() {
-        return  nameProduct + ": цена " + productPrice;
+        return nameProduct + ": цена " + productPrice;
     }
 }
 
